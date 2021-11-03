a521cf5c79095e3503c496a5ac242e8362e8ea77
AinhoaBaron <93262452+AinhoaBaron@users.noreply.github.com>
2021-11-03 14:06:45 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,43 +1,13 @@
 basic.forever(function () {
     if (input.lightLevel() < 40) {
-        basic.showLeds(`
-            . . # . .
-            . # # . .
-            . . # . .
-            . . # . .
-            . # # # .
-            `)
-    } else if (input.lightLevel() < 50) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . # # # .
-            . # . . .
-            . # # # .
-            `)
+        basic.showNumber(1)
     } else if (input.lightLevel() < 60) {
-        basic.showLeds(`
-            . # # # .
-            . . . # .
-            . . # # .
-            . . . # .
-            . # # # .
-            `)
-    } else if (input.lightLevel() < 70) {
-        basic.showLeds(`
-            . # . # .
-            . # . # .
-            . # # # .
-            . . . # .
-            . . . # .
-            `)
-    } else if (input.lightLevel() > 80) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
+        basic.showNumber(2)
+    } else if (input.lightLevel() < 80) {
+        basic.showNumber(3)
+    } else if (input.lightLevel() < 100) {
+        basic.showNumber(4)
+    } else if (input.lightLevel() > 120) {
+        basic.showNumber(5)
     }
 })
